a81a651d8abc7457f7c6ef49aac28298deb25036
Jule Marcoueille <jule.marcoueille@gmail.com>
2018-06-28 20:52:01 +0200

    Build minified version.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,61 @@
-module.exports = function(grunt) {
-    require("load-grunt-tasks")(grunt);
+module.exports = function init(grunt) {
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        clean: {
-            folder: ["tmp/"]
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      folder: ['tmp/'],
+    },
+    babel: {
+      options: {
+        sourceMap: false,
+      },
+      dist: {
+        files: {
+          'tmp/enjoyhint.js': 'src/enjoyhint.js',
+          'tmp/jquery.enjoyhint.js': 'src/jquery.enjoyhint.js',
         },
-        babel: {
-            options: {
-                sourceMap: false
-            },
-            dist: {
-                files: {
-                    "tmp/enjoyhint.js": "src/enjoyhint.js",
-                    "tmp/jquery.enjoyhint.js": "src/jquery.enjoyhint.js"
-                }
-            }
+      },
+    },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['tmp/*.js'],
+        dest: 'dist/enjoyhint.js',
+      },
+    },
+    uglify: {
+      main: {
+        files: {
+          'dist/enjoyhint.min.js': ['<%= concat.dist.dest %>'],
         },
-        concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                src: ['tmp/*.js'],
-                dest: 'dist/enjoyhint.js'
-            }
+      },
+    },
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true, flatten: true, src: ['tmp/*.map'], dest: 'dist/', filter: 'isFile',
+          },
+        ],
+      },
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'dist/enjoyhint.css': ['src/jquery.enjoyhint.css'],
         },
-        copy: {
-            main: {
-                files: [
-                    { expand: true, flatten: true, src: ['tmp/*.map'], dest: 'dist/', filter: 'isFile' }
-                ]
-            }
-        },
-        cssmin: {
-            combine: {
-                files: {
-                    'dist/enjoyhint.css': ['src/jquery.enjoyhint.css']
-                }
-            }
-        }
-    });
+      },
+    },
+  });
 
-    // grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask("default", ["babel", "concat", "copy", "cssmin", "clean"]);
+  grunt.registerTask('default', ['babel', 'concat', 'uglify', 'copy', 'cssmin', 'clean']);
 };
